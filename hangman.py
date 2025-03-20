c6d6ff80d4b17f9224baf0b3945c3b1605f16a19
Koder T <sophaline.san@gmail.com>
2025-03-20 18:09:34 +1100

    prevent user to enter the wrong guessing letter more than once

--- hangman.py
+++ hangman.py
@@ -78,34 +78,41 @@ wrong_char_list = []
 
 num_attempts = 0
 
-while num_attempts < 6:
-    matched_letter = False
 
-    # display the hangman body
-    print(hangman[num_attempts])
-    # display the space in the underscore
-    print(" ".join(masked_word), '\n')
-    print(f"Wrong letter: {', '.join(wrong_char_list)}")
-    guess_letter = input("Guess a letter: ")
-
-    for index, char in enumerate(secret_word):
-        if guess_letter == char:
-            masked_word[index] = char.capitalize()
-            matched_letter = True
-    
-    # increase the attempt if guess character is incorrect
-    if matched_letter == False:
-        num_attempts += 1
-        wrong_char_list.append(guess_letter)
-
-    # check if the words is already completed
-    if not '_' in masked_word:
+if __name__ == '__main__':
+    while num_attempts < 6:
+        matched_letter = False
+        # display the hangman body
         print(hangman[num_attempts])
         # display the space in the underscore
         print(" ".join(masked_word), '\n')
-        print(f"Yipee you won! The word is '{secret_word}'.")
-        break
 
-if num_attempts == 6:
-    print(hangman[num_attempts])
-    print(f"\nSorry you lose! :( the word was {secret_word}")
+        print(f"Wrong letter: {', '.join(wrong_char_list)}")
+
+        guess_letter = input("Guess a letter: ")
+
+        for index, char in enumerate(secret_word):
+            if guess_letter == char:
+                masked_word[index] = char.capitalize()
+                matched_letter = True
+        
+        # increase the attempt if guess character is incorrect
+        if matched_letter == False:
+            if not guess_letter in wrong_char_list:
+                num_attempts += 1
+                wrong_char_list.append(guess_letter)
+            else:
+                print("You already entered that letter!")
+
+        # check if the words is already completed
+        if not '_' in masked_word:
+            print(hangman[num_attempts])
+            # display the space in the underscore
+            print(" ".join(masked_word), '\n')
+            print(f"Yipee you won! The word is '{secret_word}'.")
+            break
+
+    if num_attempts == 6:
+        print(hangman[num_attempts])
+        print(f"\nSorry you lose! :( the word was {secret_word}")
+
